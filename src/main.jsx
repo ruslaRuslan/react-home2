934183b209089a2057378e2576d936e29ed70723
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-28 14:08:10 +0400

    oncekine nisbeten tovsiye olunan kod

--- src/main.jsx
+++ src/main.jsx
@@ -36,9 +36,34 @@ import ReactDOM from 'react-dom/client'
 // --------------------------------------------------------------------------------------------------------------------------
 
 
+// const rootElement = document.getElementById('root')
+
+// function a({imgUrl, ad}) {
+//   return <> <h1>salam {ad}</h1>
+//     <p>alekum salam </p>
+//     <img src={imgUrl} alt={`${ad}-in shiri!`} />
+//   </>
+// }
+
+// ReactDOM.createRoot(rootElement).render(
+//   <>
+//     {a({
+//       ad: "Ruslan", imgUrl: "https://images.unsplash.com/photo-1707026791615-3692b538cefa?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D"
+//     })}
+//     {a({
+//       imgUrl: "https://images.unsplash.com/photo-1706464006091-4eeb35d54bdf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D", ad: "Ali"
+//     })}
+
+//   </>
+// )
+
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+
 const rootElement = document.getElementById('root')
 
-function a({imgUrl, ad}) {
+function A({ imgUrl, ad }) {
   return <> <h1>salam {ad}</h1>
     <p>alekum salam </p>
     <img src={imgUrl} alt={`${ad}-in shiri!`} />
@@ -47,13 +72,16 @@ function a({imgUrl, ad}) {
 
 ReactDOM.createRoot(rootElement).render(
   <>
-  <a></a>
-    {a({
-      ad: "Ruslan", imgUrl: "https://images.unsplash.com/photo-1707026791615-3692b538cefa?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D"
-    })}
-    {a({
-      imgUrl: "https://images.unsplash.com/photo-1706464006091-4eeb35d54bdf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D", ad: "Ali"
-    })}
-
+    <A
+      ad="rahib"
+      imgUrl="https://images.unsplash.com/photo-1706464006091-4eeb35d54bdf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D"
+    />
+    <A
+      ad="Ruslan"
+      imgUrl="https://images.unsplash.com/photo-1707026791615-3692b538cefa?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D"
+    />
   </>
 )
+
+
+
