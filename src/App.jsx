6950bec3fa2467bd29792d58d1a083ec935d279d
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-28 14:47:45 +0400

    export default

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-export function App({ ad, imgUrl, yas }) {
+// export function App({ ad, imgUrl, yas }) {
+//     return <>
+//         <h1>salam {ad}</h1>
+//         <p>alekum salam, menim {yas} yasim var</p>
+//         <img src={imgUrl} alt={`${ad}-in shiri!`} />
+//     </>
+// }
+
+
+
+ function App({ ad, imgUrl, yas }) {
     return <>
         <h1>salam {ad}</h1>
         <p>alekum salam, menim {yas} yasim var</p>
         <img src={imgUrl} alt={`${ad}-in shiri!`} />
     </>
 }
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -116,7 +116,29 @@ import ReactDOM from 'react-dom/client'
 
 // --------------------------------------------------------------------------------------------------------------------------
 
-import {App}  from "./App"  
+// import {App}  from "./App"  
+
+// const rootElement = document.getElementById('root')
+
+// ReactDOM.createRoot(rootElement).render(
+//   <>
+//     <App
+//       ad="rahib"
+//       imgUrl="https://images.unsplash.com/photo-1706464006091-4eeb35d54bdf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D"
+//       yas={23}
+//     />
+//     <App
+//       ad="Ruslan"
+//       imgUrl="https://images.unsplash.com/photo-1707026791615-3692b538cefa?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D"
+//       yas={20}
+
+//     />
+//   </>
+// )
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+import App  from "./App"  
 
 const rootElement = document.getElementById('root')
 
