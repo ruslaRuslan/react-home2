62370517afd82c4c6d4ec094a0f929584a43369d
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-28 14:10:46 +0400

    yas teyin etdik

--- src/main.jsx
+++ src/main.jsx
@@ -63,9 +63,9 @@ import ReactDOM from 'react-dom/client'
 
 const rootElement = document.getElementById('root')
 
-function A({ imgUrl, ad }) {
+function A({ imgUrl, ad, yas }) {
   return <> <h1>salam {ad}</h1>
-    <p>alekum salam </p>
+    <p>alekum salam, menim {yas} yasim var</p>
     <img src={imgUrl} alt={`${ad}-in shiri!`} />
   </>
 }
@@ -75,10 +75,13 @@ ReactDOM.createRoot(rootElement).render(
     <A
       ad="rahib"
       imgUrl="https://images.unsplash.com/photo-1706464006091-4eeb35d54bdf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D"
+      yas={23}
     />
     <A
       ad="Ruslan"
       imgUrl="https://images.unsplash.com/photo-1707026791615-3692b538cefa?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D"
+      yas={20}
+
     />
   </>
 )
