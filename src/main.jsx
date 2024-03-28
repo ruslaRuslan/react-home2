0d13eb86bd3dcb1b8564a8cf174856a0a7fcc49b
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-28 12:36:53 +0400

    a teqine menimsetdid ve render eledik

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 const rootElement = document.getElementById('root')
 
+const a = <div>
+  <h1>salam</h1>
+  <p>alekum salam</p>
+  <img src="https://images.unsplash.com/photo-1709828593320-8af956395dd1?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+</div>
+
 ReactDOM.createRoot(rootElement).render(
-  <div>
-    <h1> salam </h1>
-    <p>alekum salam</p>
-    <img src="https://images.unsplash.com/photo-1709828593320-8af956395dd1?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-  </div>
+a
 )
