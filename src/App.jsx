366ab0f01bb9e3cf1e6f04c7b7102105aa82be28
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-28 14:27:13 +0400

    named export

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export function App({ ad, imgUrl, yas }) {
+    return <>
+        <h1>salam {ad}</h1>
+        <p>alekum salam, menim {yas} yasim var</p>
+        <img src={imgUrl} alt={`${ad}-in shiri!`} />
+    </>
+}
