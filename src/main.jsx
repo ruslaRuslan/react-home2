6ee989007fd3cf6853487aed3b06b1f6cac23b96
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-28 12:35:18 +0400

    img, p, h1

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ const rootElement = document.getElementById('root')
 
 ReactDOM.createRoot(rootElement).render(
   <div>
-    <h1>salam</h1>
+    <h1> salam </h1>
     <p>alekum salam</p>
     <img src="https://images.unsplash.com/photo-1709828593320-8af956395dd1?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
   </div>
