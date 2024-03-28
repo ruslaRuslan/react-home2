6625903a9572eb0e9a5174902ce53d87258ef73b
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-28 14:13:00 +0400

    destructure-ni bilmeyebler ucun props

--- src/main.jsx
+++ src/main.jsx
@@ -61,12 +61,40 @@ import ReactDOM from 'react-dom/client'
 // --------------------------------------------------------------------------------------------------------------------------
 
 
+// const rootElement = document.getElementById('root')
+
+// function A({ imgUrl, ad, yas }) {
+//   return <> <h1>salam {ad}</h1>
+//     <p>alekum salam, menim {yas} yasim var</p>
+//     <img src={imgUrl} alt={`${ad}-in shiri!`} />
+//   </>
+// }
+
+// ReactDOM.createRoot(rootElement).render(
+//   <>
+//     <A
+//       ad="rahib"
+//       imgUrl="https://images.unsplash.com/photo-1706464006091-4eeb35d54bdf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D"
+//       yas={23}
+//     />
+//     <A
+//       ad="Ruslan"
+//       imgUrl="https://images.unsplash.com/photo-1707026791615-3692b538cefa?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D"
+//       yas={20}
+
+//     />
+//   </>
+// )
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+
 const rootElement = document.getElementById('root')
 
-function A({ imgUrl, ad, yas }) {
-  return <> <h1>salam {ad}</h1>
-    <p>alekum salam, menim {yas} yasim var</p>
-    <img src={imgUrl} alt={`${ad}-in shiri!`} />
+function A(props) {
+  return <> <h1>salam {props.ad}</h1>
+    <p>alekum salam, menim {props.yas} yasim var</p>
+    <img src={props.imgUrl} alt={`${props.ad}-in shiri!`} />
   </>
 }
 
@@ -85,6 +113,5 @@ ReactDOM.createRoot(rootElement).render(
     />
   </>
 )
-
 
 
